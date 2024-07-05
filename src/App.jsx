2cdd59e8667fce57ca2f7e4d53a371a4ab01bbb3
Hariom dangi <hdangi224@gmail.com>
2024-07-05 19:47:32 +0530

    component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "./components/Card";
+
+const App = () => {
+  return (
+    <div className="flex">
+      <Card />
+      <Card />
+      
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="h-96 w-96 bg-red-500 m-10 flex items-center justify-center">
+      <h1 className="text-white font-bold">Card</h1>
+    </div>
+  );
+};
+
+export default Card;
